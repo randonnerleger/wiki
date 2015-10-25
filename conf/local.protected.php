4cf94ebaa7b73fbae34f2a6da6e4a9cb170c545e
oli_v_ier <oli_v_ier@yahoo.fr>
2015-10-24 22:25:49 -0400

    Ajout des fichiers de configuration

---
+++ conf/local.protected.php
@@ -0,0 +1,8 @@
+<?php
+define('PUN_ROOT', dirname(__FILE__).'/../../forum_new/');
+include PUN_ROOT.'include/common.php';
+ 
+$conf['useacl'] = 1;
+$conf['superuser'] = '@Administrateurs';
+$conf['authtype'] = 'authfluxbb';
+$conf['disableactions'] = 'register,resendpwd,profile,login,logout';
